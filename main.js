21682af82af11df32b60a7daa0ae94906fe1453e
lokesh932338 <142098970+lokesh932338@users.noreply.github.com>
2024-01-02 23:59:16 +0530

    trying to fix the smartphone wall height

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ window.onload = function(){
                     margin: 0;
                     padding: 0;
                     grid-template-columns: 1fr;
-                    height: ${screenHeight};
+                    // height: ${screenHeight};
                 }
             
                 .taskContainer {
